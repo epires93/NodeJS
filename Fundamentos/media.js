641dbdfc48b23e70a6ab8f7a60fc6aecb8510655
Erica Pires <116045362+epires93@users.noreply.github.com>
2023-08-09 22:19:27 -0300

    Add files via upload

---
+++ Fundamentos/media.js
@@ -0,0 +1,30 @@
+/**
+ * App para calcular a média de 2 notas
+ */
+
+
+let nome, disciplina
+let nota1, nota2, media
+
+console.clean
+console.log("/ \\ / \\ / \\ / \\ / \\ / \\ / \\")
+console.log(" B | o | l | e | t | i | m ")
+console.log("\\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/")
+console.log(" ")
+
+// Entrada
+nome = "Érica S."
+disciplina = "Matemática"
+nota1 = 9
+nota2 = 8
+
+// Processamento
+media = (nota1 + nota2) / 2
+
+
+// Saída
+console.log(`Nome: ${nome}`)
+console.log(`Disciplina: ${disciplina}`)
+console.log(`Nota 1: ${nota1}`)
+console.log(`Nota 2: ${nota2}`)
+console.log(`Média Final: ${media}`)
